90ae0724bfcaa43741fdb27de7f057a1edf25036
CarloD39 <carlodurand@hotmail.it>
2021-09-22 14:32:34 +0200

    6

---
+++ snack6.php
@@ -0,0 +1,75 @@
+<!-- Utilizzare questo array: https://pastebin.com/CkX3680A.
+     Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+
+
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+
+    ?>
+
+    <div style=" background-color:gainsboro; ">
+
+        <?php
+
+        for($i = 0; $i < count($db['teachers']); $i++) {
+
+            echo "{$db['teachers'][$i]['name']} {$db['teachers'][$i]['lastname']} <br/>";
+        }
+
+        ?>
+
+    </div>
+
+    <div style=" background-color:forestgreen; ">
+
+        <?php
+
+        for($i = 0; $i < count($db['pm']); $i++) {
+
+            echo "{$db['pm'][$i]['name']} {$db['pm'][$i]['lastname']} <br/>";
+        }
+
+        ?>
+
+    </div>
+    
+</body>
+</html>
+
+
+
