c7e0b51b63b455cec8dec1613b23e4397992109a
CarloD39 <carlodurand@hotmail.it>
2021-09-22 10:02:30 +0200

    snack1

---
+++ snack1.php
@@ -0,0 +1,32 @@
+<?php
+
+$matches = [
+    
+    [
+        
+        "casa" => "Virtus Roma",
+        "ospite" => "Fortitudo Bologna",
+        "puntiCasa" => 90,
+        "puntiOspite" => 100
+    ],
+    [
+        "casa" => "Brescia",
+        "ospite" => "Brindisi",
+        "puntiCasa" => 80,
+        "puntiOspite" => 95
+    ],
+    [
+        "casa" => "Trieste",
+        "ospite" => "Treviso",
+        "puntiCasa" => 98,
+        "puntiOspite" => 92
+
+    ]
+    
+    
+];
+
+for($i = 0; $i < count($matches); $i++){
+
+    echo "{$matches[$i] ['casa']} - {$matches[$i] ['ospite']} - {$matches[$i] ['puntiCasa']} - {$matches[$i] ['puntiOspite']} " ;
+}
